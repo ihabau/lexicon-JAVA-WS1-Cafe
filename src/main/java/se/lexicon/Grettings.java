31ee20ee1a74bce17a91eade852b243beb6a7edf
ihab Al Ubaidi <www.ihabau@gmail.com>
2026-06-11 13:34:51 +0200

    Cafe terminal Shop. finnished mandetory part remaining error handling of input

--- src/main/java/se/lexicon/Grettings.java
+++ src/main/java/se/lexicon/Grettings.java
@@ -4,36 +4,40 @@ import java.util.Scanner;
 public class Grettings {
     public static String userName;
     public static String devider =  "|===================================================|";
+    public static int totalSpace = 48;
+    public static int leftSpace;
+    public static int rightSpace;
 
     public static void Gretings() {
-        String menuGreeting = "Here is the menu:";
         System.out.println( devider);
         System.out.println("|                  LEXICON CAFE                     |");
         System.out.println( devider);
         System.out.println("| Welcome! What is your name?                       |");
         Scanner scanner = new Scanner(System.in);
         userName = scanner.nextLine();
-        int nameLength = userName.length();
-        int spacing = 44;
-        spacing -= nameLength;
-        String dynamicFormat = "|Hi %s %-" + spacing + "s|\n";
+        String message = "Hi " + userName + "! here is the menu:";
+        rightSpace = totalSpace - message.length() + 1;
+        String dynamicFormat = "| %s %" + rightSpace + "s|\n";
         System.out.println( devider);
-        System.out.printf(dynamicFormat, userName, menuGreeting);
+        System.out.printf(dynamicFormat, message, "");
         System.out.println( devider);
     }
 
     public static void thankYou() {
     System.out.println(devider);
 
-        int nameLength = userName.length();
-        int spacing =  (int) Math.ceil((47 - nameLength) / 2);
-        System.out.println(spacing);
-        String dynamicFormat = "|%" + spacing +"s Thank you, %-10s %"+ spacing + "s|\n";
+        String message = "Thank you, " + userName;
+
+        leftSpace = (int) Math.ceil((48 - message.length()) / 2.0);;
+        rightSpace = totalSpace - (leftSpace + message.length());
+
+        System.out.println(leftSpace);
+        String dynamicFormat = "|%" + leftSpace + "s %s %" + rightSpace + "s |\n";
 
 
         System.out.println(devider);
         //System.out.printf("|%16.16s Thank you, %s! %17.17s|\n","", userName, "");
-        System.out.printf(dynamicFormat,"", userName, "");
+        System.out.printf(dynamicFormat,"", message, "");
         System.out.printf("|%18.18s See you again. %17.17s|\n","", "");
         System.out.println(devider);
     }
